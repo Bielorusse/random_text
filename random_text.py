cf0ee754ce9c15eec9a0b8516a336e7a81f90cf7
Bielorusse <thibautvoirand@gmail.com>
2018-08-06 23:10:11 +0200

    initial commit

---
+++ random_text.py
@@ -0,0 +1,166 @@
+"""
+Mix the words of a source text to create a random one.
+
+Version 0.1 of the 20180806.
+"""
+
+import re
+import random
+
+def clean_latex_text(input_file):
+    """
+    Reads text file containing latex text and clears everything linked to latex formatting.
+
+    Input:
+    -input_file         string
+    Output:
+    -output_contents    list of strings
+    """
+
+    output_contents = []
+
+    with open(input_file, "r") as file:
+
+        input_contents = file.readlines()
+
+    for row in input_contents:
+
+        for word in row.split():
+
+            if not word.startswith("\\") and not word.startswith("%"):
+
+                output_contents.append(word)
+
+    for i, row in enumerate(output_contents):
+
+        output_contents[i] = row.replace("}", "")
+
+    return output_contents
+
+def remove_uppercase(input_words):
+    """
+    Remove the uppercase of the beginning of sentences in a list of words.
+
+    Input:
+    -input_words    list of strings
+    Ouput:
+    -output_words   list of strings
+    """
+
+    output_words = [input_words[0]]
+
+    for i in range(len(input_words[:-1])):
+
+        if input_words[i].endswith("."):
+
+            output_words.append(input_words[i+1].lower())
+
+        else:
+
+            output_words.append(input_words[i+1])
+
+    return output_words
+
+def add_uppercase(input_words):
+    """
+    Remove the uppercase of the beginning of sentences in a list of words.
+
+    Input:
+    -input_words    list of strings
+    Ouput:
+    -output_words   list of strings
+    """
+
+    output_words = [input_words[0].capitalize()]
+
+    for i in range(len(input_words[:-1])):
+
+        if input_words[i].endswith("."):
+
+            output_words.append(input_words[i+1].capitalize())
+
+        else:
+
+            output_words.append(input_words[i+1])
+
+    output_words[-1] = "{}.".format(output_words[-1])
+
+    return output_words
+
+def randomize_list_of_words(input_words):
+    """
+    Returns the same list of words in a random order.
+
+    Input:
+    -input_words    list of strings
+    Output:
+    -output_words   list of strings
+    """
+
+    output_words = []
+
+    random_order = random.sample(range(len(input_words)), len(input_words))
+
+    for i in random_order:
+
+        output_words.append(input_words[i])
+
+    return output_words
+
+def random_newlines(words_list, nb_of_newlines):
+    """
+    Distribute randomly a given number of newline characters in a list of words.
+
+    Input:
+    -words_list         list of strings
+    -nb_of_newlines     integer
+    Output:
+    -words_list         list of strings
+    """
+
+    period_indexes = []
+
+    for i, word in enumerate(words_list):
+
+        if "." in word:
+
+            period_indexes.append(i + 1)
+
+    if nb_of_newlines > len(period_indexes):
+        nb_of_newlines = len(period_indexes)
+
+    newline_indexes = random.sample(period_indexes, nb_of_newlines)
+
+    for index in newline_indexes:
+
+        words_list.insert(index, "\n\n")
+
+    return words_list
+
+if __name__ == "__main__":
+
+    source_words = clean_latex_text("source_text2.txt")
+
+    source_words_wo_uppercase = remove_uppercase(source_words)
+
+    random_words_wo_uppercase = randomize_list_of_words(source_words_wo_uppercase)
+
+    random_words = add_uppercase(random_words_wo_uppercase)
+
+    with open("random_text1.txt", "w") as file:
+
+        for word in random_words:
+
+            file.write(
+                "{} ".format(word)
+            )
+
+    random_words = random_newlines(random_words, 3)
+
+    with open("random_text2.txt", "w") as file:
+
+        for word in random_words:
+
+            file.write(
+                "{} ".format(word)
+            )
---
+++ readme.txt
@@ -0,0 +1,5 @@
+Random text.
+
+Mix the words of a source text to create a random one.
+
+Version 0.1 of the 20180806.
